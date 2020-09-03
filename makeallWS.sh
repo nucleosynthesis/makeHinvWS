5110b26ca43ad944f42922591a3574287eb1c4d3
Samuel Webb <samuel.webb@cern.ch>
2020-09-03 12:15:20 +0200

    removing specific path

--- makeallWS.sh
+++ makeallWS.sh
@@ -1,5 +1,4 @@
 cmsenv
-LD_LIBRARY_PATH=~/work/private/Hinv/workspace/CMSSW_10_2_13/src/HiggsAnalysis/CombinedLimit/lib:$LD_LIBRARY_PATH
 cd test_df_MTR_2017_2020v1
 root -b '../makeallWS.C("2017","MTR")'
 cd ../test_df_MTR_2018_2020v1
